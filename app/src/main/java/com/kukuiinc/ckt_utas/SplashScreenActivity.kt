5176ef9c70c42732cec9222d62fc729ac270b0a8
raymondwix <brahwix@gmail.com>
2022-11-19 19:28:59 -1200

    Initial commit

---
+++ app/src/main/java/com/kukuiinc/ckt_utas/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.kukuiinc.ckt_utas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private var splashScreenTimeout : Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },
+            splashScreenTimeout
+        )
+
+    }
+}
